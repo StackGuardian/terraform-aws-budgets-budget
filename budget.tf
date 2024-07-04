428d95c2e29b89f8e99ee3e20c07d87ae0059e3d
Johannes Scheuerer <joscheuerer@gmail.com>
2024-07-04 22:28:38 +0200

    Update budget.tf

--- budget.tf
+++ budget.tf
@@ -1,7 +1,7 @@
 resource "aws_budgets_budget" "total" {
   name              = "total-budget-monthly"
   budget_type       = "COST"
-  limit_amount      = var.total-limit
+  limit_amount      = var.total_limit
   limit_unit        = "USD"
   time_period_end   = "2087-06-15_00:00"
   time_period_start = "2017-07-01_00:00"
